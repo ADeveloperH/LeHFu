983bbc94a0cf0f27b2a7120a1ca16c02ac37106c
huangjian <huangjian1@hydsoft.com>
2018-01-14 15:57:51 +0800

    密码登录页面布局完成

---
+++ app/src/main/java/com/jiefutong/lehfu/activity/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.jiefutong.lehfu.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.jiefutong.lehfu.R;
+import com.jiefutong.lehfu.base.BaseNotTitleActivity;
+
+/**
+ * author：hj
+ * time: 2018/1/14 0014 15:28
+ * description:
+ */
+
+
+public class LoginActivity extends BaseNotTitleActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

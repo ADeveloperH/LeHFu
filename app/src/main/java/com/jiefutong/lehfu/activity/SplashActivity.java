d35e8924a1e41b3e11f0c7d383664674e867833e
huangjian <huangjian1@hydsoft.com>
2018-01-14 16:36:45 +0800

    找回密码页面布局完成

--- app/src/main/java/com/jiefutong/lehfu/activity/SplashActivity.java
+++ app/src/main/java/com/jiefutong/lehfu/activity/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends BaseNotTitleActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_layout);
 
-        startActivity(new Intent(this,SettingPwdActivity.class));
+        startActivity(new Intent(this,FindPwdActivity.class));
         finish();
     }
 }
